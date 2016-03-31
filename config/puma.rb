1994257609a063d65f9dc7da394c4ae93b78d3cb
Simon Waddington <simon@talkdesk.com>
2016-03-31 16:55:44 -0700

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,9 @@
-threads 2, 2
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-port ENV['PORT'] || 4567
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
